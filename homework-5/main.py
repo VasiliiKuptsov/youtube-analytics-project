829416c76a901518d174863159ffac9c5dfc15f9
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-08-06 10:39:58 +0300

    100 proc

--- homework-5/main.py
+++ homework-5/main.py
@@ -4,14 +4,12 @@ from src.video import PlayList
 
 if __name__ == '__main__':
     pl = PlayList('PLekfpYe-gBR88aQVAL76u3FPJ8hpeRkz1')
-    #print (pl)
 
     assert pl.title == "Дима..."
     assert pl.url == "https://www.youtube.com/playlist?list=PLekfpYe-gBR88aQVAL76u3FPJ8hpeRkz1"
 
     duration = pl.total_duration
-    assert str(duration) == "1:49:52"
+    assert str(duration) == "6:28:55"
     assert isinstance(duration, datetime.timedelta)
-    assert duration.total_seconds() == 6592.0
-
-    assert pl.show_best_video() == "https://youtu.be/cUGyMzWQcGM"
+    assert duration.total_seconds() == 23335.0
+    assert pl.show_best_video() == "https://youtu.be/vwzIXP9k6_8"
--- src/video.py
+++ src/video.py
@@ -1,5 +1,14 @@
 from src.channel import youtube
-import datetime
+import datetime, isodate
+
+playlist_id = 'PLekfpYe-gBR88aQVAL76u3FPJ8hpeRkz1'
+playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+                                               part='contentDetails',
+                                               maxResults=50,
+                                               ).execute()
+video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+
+
 class Video:
 
 
@@ -27,44 +36,41 @@ class PLVideo(Video):
 
 
 class PlayList(Video):
+
+    playlist_id = 'PLekfpYe-gBR88aQVAL76u3FPJ8hpeRkz1'
+    playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+                                                   part='contentDetails',
+                                                   maxResults=50,
+                                                   ).execute()
+    video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+
+
     def __init__(self,playlist_id):
         self.__playlist_id = playlist_id
         self.title = 'Дима...'
         self.url = 'https://www.youtube.com/playlist?list=PLekfpYe-gBR88aQVAL76u3FPJ8hpeRkz1'
+
     @property
 
 
     def total_duration(self):
         add_time = datetime.timedelta(minutes = 0)
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                                id=','.join(video_ids)
+                                                ).execute()
         for video in video_response['items']:
-            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                   id=video_id
-                                                   ).execute()
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
-        print(duration)
-        add_time += duration
+            add_time += duration
+
         return add_time
 
 
 
     def show_best_video(self):
 
-        playlist_id = 'PLekfpYe-gBR88aQVAL76u3FPJ8hpeRkz1'
-        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
-                                                       part='contentDetails',
-                                                       maxResults=50,
-                                                       ).execute()
-        # print(playlist_videos)
         like: int = 0
-        print(type(like))
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        # print(video_ids)
         for video_id in video_ids:
-            print(video_id)
             video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                    id=video_id
                                                    ).execute()
